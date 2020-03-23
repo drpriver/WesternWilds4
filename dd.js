f888e9467049af092d0b336b035e1c4436b930de
drpriver <priver.david@gmail.com>
2020-03-23 00:30:27 -0400

    Chapters

--- dd.js
+++ dd.js
@@ -1,5 +1,12 @@
 "use strict"
 function onload(){
+    const chapters = document.getElementsByClassName("chapter");
+    for(let i = 0; i < chapters.length; i++){
+        const chapter = chapters[i];
+        const el = document.createElement("hr");
+        chapter.parentNode.insertBefore(el, chapter.nextSibling);
+        console.log(i);
+    }
 }
 document.addEventListener("DOMContentLoaded", onload)
 
